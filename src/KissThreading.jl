8675f55ef9469fccf808a44237bd5f0bbb02b950
Bogumił Kamiński <bkamins@sgh.waw.pl>
2018-01-08 22:13:11 +0100

    fix tid

--- src/KissThreading.jl
+++ src/KissThreading.jl
@@ -7,7 +7,8 @@ function trandjump(rng = Base.GLOBAL_RNG)
     rngjmp = randjump(rng, n+1)
     rngs = Vector{MersenneTwister}(n)
     Threads.@threads for i in 1:n
-        rngs[Threads.threadid()] = deepcopy(rngjmp[tid+1])
+        tid = Threads.threadid()
+        rngs[tid] = deepcopy(rngjmp[tid+1])
     end
     all([isassigned(rngs, i) for i in 1:n]) || error("failed to create rngs")
     rngs
