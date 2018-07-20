32c67e2fa08f1e8ef0d931915f94d92034bf3fe9
Bogumił Kamiński <bkamins@sgh.waw.pl>
2018-07-20 21:26:50 +0200

    code clearup of tmapreduce and remove tmapadd

--- src/KissThreading.jl
+++ src/KissThreading.jl
@@ -16,6 +16,8 @@ end
 
 const TRNG = trandjump()
 
+default_batch_size(n) = min(n, round(Int, 10*sqrt(n)))
+
 function tmap!(f::Function, dst::AbstractVector, src::AbstractVector)
     ld = length(dst)
     if ld != length(src)
@@ -54,141 +56,63 @@ function tmap!(f::Function, dst::AbstractVector, src::AbstractVector...)
     end
 end
 
-default_batch_size(n) = min(n, round(Int, 10*sqrt(n)))
-
 # we assume that f(src) is a subset of Abelian group with op
-# and that v0 is its identity
-
-function tmapreduce(f::Function, op::Function, v0::T, src::AbstractVector; batch_size=default_batch_size(length(src))) where T 
-    r = deepcopy(v0)
+function tmapreduce(f::Function, op::Function, src::AbstractVector; init,
+                    batch_size=default_batch_size(length(src)))
+    r = deepcopy(init)
     i = Threads.Atomic{Int}(1)
     l = Threads.SpinLock()
     ls = length(src)
-    nt = Threads.nthreads()
-    return let r::T = r;
-        Threads.@threads for j in 1:nt
-            k = Threads.atomic_add!(i, batch_size)
-            if k <= ls
-                x = f(src[k])
-                range = (k+1):min(k+batch_size-1, ls)
-                for idx in range
-                    x = op(x, f(src[idx]))
-                end
-            else
-                continue
-            end
-            k = Threads.atomic_add!(i, batch_size)
-            while k ≤ ls
-                range = k:min(k+batch_size-1, ls)
-                for idx in range
-                    x = op(x, f(src[idx]))
-                end
-                k = Threads.atomic_add!(i, batch_size)
-            end
-            Threads.lock(l)
-            r = op(r, x)
-            Threads.unlock(l)
+    Threads.@threads for j in 1:Threads.nthreads()
+        k = Threads.atomic_add!(i, batch_size)
+        k > ls && continue
+        x = f(src[k])
+        for idx in (k+1):min(k+batch_size-1, ls)
+            x = op(x, f(src[idx]))
         end
-        r
+        k = Threads.atomic_add!(i, batch_size)
+        while k ≤ ls
+            for idx in k:min(k+batch_size-1, ls)
+                x = op(x, f(src[idx]))
+            end
+            k = Threads.atomic_add!(i, batch_size)
+        end
+        Threads.lock(l)
+        r = op(r, x)
+        Threads.unlock(l)
     end
+    r
 end
 
-function tmapreduce(f::Function, op::Function, v0::T, src::AbstractVector...; batch_size=default_batch_size(length(src[1]))) where T
+function tmapreduce(f::Function, op::Function, src::AbstractVector...; init,
+                    batch_size=default_batch_size(length(src[1])))
     lss = extrema(length.(src))
     lss[1] == lss[2] || throw(ArgumentError("src vectors must have the same length"))
 
-    r = deepcopy(v0)
+    r = deepcopy(init)
     i = Threads.Atomic{Int}(1)
     l = Threads.SpinLock()
     ls = lss[1]
-    nt = Threads.nthreads()
-    return let r::T = r;
-        Threads.@threads for j in 1:nt
-            k = Threads.atomic_add!(i, batch_size)
-            if k <= ls
-                x = f(getindex.(src, k)...)
-                range = (k+1):min(k+batch_size-1, ls)
-                for idx in range
-                    x = op(x, f(getindex.(src, idx)...))
-                end
-            else
-                continue
-            end
-            k = Threads.atomic_add!(i, batch_size)
-            while k ≤ ls
-                range = k:min(k+batch_size-1, ls)
-                for idx in range
-                    x = op(x, f(getindex.(src, idx)...))
-                end
-                k = Threads.atomic_add!(i, batch_size)
-            end
-            Threads.lock(l)
-            r = op(r, x)
-            Threads.unlock(l)
-        end
-        r
-    end
-end
 
-function tmapadd(f::Function, v0::T, src::AbstractVector; batch_size=default_batch_size(length(src))) where T
-    r = Threads.Atomic{T}(deepcopy(v0))
-    i = Threads.Atomic{Int}(1)
-    ls = length(src)
-    nt = Threads.nthreads()
-    Threads.@threads for j in 1:nt
+        Threads.@threads for j in 1:Threads.nthreads()
         k = Threads.atomic_add!(i, batch_size)
-        if k <= ls
-            x = f(src[k])
-            range = (k+1):min(k+batch_size-1, ls)
-            for idx in range
-                x += f(src[idx])
-            end
-        else
-            continue
+        k > ls && continue
+        x = f(getindex.(src, k)...)
+        for idx in (k+1):min(k+batch_size-1, ls)
+            x = op(x, f(getindex.(src, idx)...))
         end
         k = Threads.atomic_add!(i, batch_size)
         while k ≤ ls
-            range = k:min(k+batch_size-1, ls)
-            for idx in range
-                x += f(src[idx])
+            for idx in k:min(k+batch_size-1, ls)
+                x = op(x, f(getindex.(src, idx)...))
             end
             k = Threads.atomic_add!(i, batch_size)
         end
-        Threads.atomic_add!(r, x)
+        Threads.lock(l)
+        r = op(r, x)
+        Threads.unlock(l)
     end
-    return r[]
-end
-
-function tmapadd(f::Function, v0::T, src::AbstractVector...; batch_size=default_batch_size(length(src[1]))) where T
-    lss = extrema(length.(src))
-    lss[1] == lss[2] || throw(ArgumentError("src vectors must have the same length"))
-
-    r = Threads.Atomic{T}(deepcopy(v0))
-    i = Threads.Atomic{Int}(1)
-    ls = lss[1]
-    nt = Threads.nthreads()
-    Threads.@threads for j in 1:nt
-        k = Threads.atomic_add!(i, batch_size)
-        if k <= ls
-            x = f(getindex.(src, k)...)
-            range = (k+1):min(k+batch_size-1, ls)
-            for idx in range
-                x += f(getindex.(src, idx)...)
-            end
-        else
-            continue
-        end
-        k = Threads.atomic_add!(i, batch_size)
-        while k ≤ ls
-            range = k:min(k+batch_size-1, ls)
-            for idx in range
-                x += f(getindex.(src, idx)...)
-            end
-            k = Threads.atomic_add!(i, batch_size)
-        end
-        Threads.atomic_add!(r, x)
-    end
-    return r[]
+    r
 end
 
 function getrange(n)
