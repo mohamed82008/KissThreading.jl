34c218f00d9c9709f6ce991a4ccfb1708a292e77
Bogumił Kamiński <bkamins@sgh.waw.pl>
2018-07-21 12:58:27 +0200

    fix tmapreduce tests

--- test/mapreduce.jl
+++ test/mapreduce.jl
@@ -3,17 +3,17 @@
     n = 10000000
     a = rand(n)
     println("threaded tmapreduce: $(Threads.nthreads()) threads")
-    tmapreduce(log, +, 0., a)
-    @time tmapreduce(log, +, 0., a)
+    tmapreduce(log, +, a, init=0.0)
+    @time tmapreduce(log, +, a, init=0.0)
 
     println("unthreaded")
     mapreduce(log, +, a, init=0.)
     @time mapreduce(log, +, a, init=0.)
 
-    @test tmapreduce(log, +, 0., ones(1000)) == 0
+    @test tmapreduce(log, +, ones(1000), init=0.0) == 0
     srand(1234321)
     a = rand(1000)
-    r1 = tmapreduce((x)->(2*x), +, 0., a)
+    r1 = tmapreduce((x)->(2*x), +, a, init=0.0)
     r2 = mapreduce((x)->(2*x), +, a, init=0.)
-    @test r1 ≈ r2
+    r1 ≈ r2
 end
